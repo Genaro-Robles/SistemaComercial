27c1d6e0776a50f98e11d97a865d1e5e0e3e1222
Rexar151 <rqgenaro_junior2001@yahoo.es>
2022-06-20 13:06:58 -0500

    V2.1

--- ProyectoFinal/Properties/AssemblyInfo.cs
+++ ProyectoFinal/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("ProyectoFinal")]
+[assembly: AssemblyTitle("CapaVista")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProyectoFinal")]
+[assembly: AssemblyProduct("CapaVista")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
